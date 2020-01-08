8392922d7820c6b94549b5df013fb9249a935bff
Martin <tenzin.braun@gmail.com>
2020-01-08 16:54:50 +0100

    [TASK] perform scroll & Update style

--- app/src/main/java/fr/peaky/photographieproject/ui/adapter/PelliculeAdapter.kt
+++ app/src/main/java/fr/peaky/photographieproject/ui/adapter/PelliculeAdapter.kt
@@ -7,6 +7,7 @@ import androidx.recyclerview.widget.RecyclerView
 import fr.peaky.photographieproject.R
 import fr.peaky.photographieproject.data.model.Pellicule
 import fr.peaky.photographieproject.ui.activity.PelliculeDetailActivity
+import fr.peaky.photographieproject.ui.activity.PelliculeListActivity
 import fr.peaky.photographieproject.ui.component.inflate
 import kotlinx.android.synthetic.main.pellicule_item_holder.view.*
 
@@ -57,3 +58,21 @@ class PelliculeViewHolder(view: View) : RecyclerView.ViewHolder(view) {
         const val PELLICULE_EXTRA_KEY = "pellicule_extra_key"
     }
 }
+
+class CustomScrollListener(pelliculeListActivity: PelliculeListActivity) :
+    RecyclerView.OnScrollListener() {
+
+    private val pelliculeListActivity = pelliculeListActivity
+
+    override fun onScrollStateChanged(recyclerView: RecyclerView, newState: Int) {
+    }
+
+    override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+        when {
+            dy > 0 -> pelliculeListActivity.notifyMovingScroll(1)
+            dy < 0 -> pelliculeListActivity.notifyMovingScroll(2)
+            else -> pelliculeListActivity.notifyMovingScroll(0)
+        }
+    }
+}
+
