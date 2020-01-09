f3556420419cdcf969f22c857e4e8c00d5045eb2
sylv42240 <sylv42240@gmail.com>
2020-01-09 11:16:20 +0100

    [TASK] Update PelliculeListActivity

--- app/src/main/java/fr/peaky/photographieproject/ui/activity/PelliculeListActivity.kt
+++ app/src/main/java/fr/peaky/photographieproject/ui/activity/PelliculeListActivity.kt
@@ -1,10 +1,11 @@
 package fr.peaky.photographieproject.ui.activity
 
+import android.app.Dialog
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
-import android.widget.ArrayAdapter
-import android.widget.Spinner
-import android.widget.Toast
+import android.view.ViewGroup
+import android.widget.*
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -32,23 +33,30 @@ class PelliculeListActivity : AppCompatActivity() {
     private val db = FirebaseFirestore.getInstance()
     private val adapter = PelliculeAdapter()
     private val userId = FirebaseAuth.getInstance().currentUser?.uid
+    private lateinit var alertDialog: Dialog
+    private val pelliculeList = mutableListOf<Pellicule>()
+    private var noElement = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_pellicule_list)
+        alertDialog = Dialog(this)
         val view: View = findViewById(android.R.id.content)
         getDatabaseInfos(view)
+        researchFabMenuBar.setOnClickListener {
+            showAddPelliculeDialog()
+        }
     }
 
 
     private fun getDatabaseInfos(view: View) {
+        pelliculeList.clear()
         if (isOnline(this)) {
             db.collection(PELLICULE_VALUE)
                 .whereEqualTo(USER_PARAMETER, userId)
                 .get()
                 .addOnCompleteListener { task ->
                     if (task.isSuccessful) {
-                        val pelliculeList = mutableListOf<Pellicule>()
                         task.result?.map { document ->
                             val pellicule = document.toObject(Pellicule::class.java)
                             pelliculeList.add(pellicule)
@@ -71,6 +79,7 @@ class PelliculeListActivity : AppCompatActivity() {
     private fun updateBackground() {
         pellicule_list_progress_bar.hide()
         empty_pellicule_layout.show()
+        noElement = true
     }
 
     private fun updateRecyclerView(pelliculeList: MutableList<Pellicule>) {
@@ -82,7 +91,7 @@ class PelliculeListActivity : AppCompatActivity() {
         pellicule_list_progress_bar.hide()
     }
 
-    fun notifyMovingScroll(responseCode: Int){
+    fun notifyMovingScroll(responseCode: Int) {
         when (responseCode) {
             1 -> {
                 myBottomAppBar.performHide()
@@ -96,18 +105,85 @@ class PelliculeListActivity : AppCompatActivity() {
     }
 
 
-
-    /*
-     TODO YKR
-    fun showDialog(view: View){
-        val spinner = Spinner(this)
-        val userNames = arrayOf("50", "100", "200", "400", "800", "1 600", "12 800", "25 600")
-        val arrayadapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, userNames)
-        spinner.adapter = arrayadapter
-        val alertDialogBuilder = AlertDialog.Builder(this)
-        alertDialogBuilder.setView(spinner)
-        val alertDialog = alertDialogBuilder.create()
+    private fun showAddPelliculeDialog() {
+        val viewGroup = findViewById<ViewGroup>(android.R.id.content)
+        val dialogView =
+            LayoutInflater.from(this).inflate(R.layout.create_pellicule_dialog, viewGroup, false)
+        val buttonValidate = dialogView.findViewById<Button>(R.id.btn_validate)
+        val buttonCancel = dialogView.findViewById<Button>(R.id.btn_cancel)
+        val spinner = dialogView.findViewById<Spinner>(R.id.pellicule_iso_spinner)
+        val spinnerList = arrayOf(
+            "ISO 50",
+            "ISO 100",
+            "ISO 200",
+            "ISO 400",
+            "ISO 800",
+            "ISO 1 600",
+            "ISO 12 800",
+            "ISO 25 600"
+        )
+        val arrayAdapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, spinnerList)
+        val editText = dialogView.findViewById<EditText>(R.id.pellicule_name)
+        spinner.adapter = arrayAdapter
+        buttonValidate.setOnClickListener {
+            if (addPelliculeToFirestore(
+                    editText.text.toString(),
+                    spinner.selectedItem.toString()
+                )
+            ) {
+                alertDialog.dismiss()
+            }
+        }
+        buttonCancel.setOnClickListener {
+            alertDialog.dismiss()
+        }
+        val builder = AlertDialog.Builder(this)
+        builder.setView(dialogView)
+        alertDialog = builder.create()
+        alertDialog.setCancelable(false)
+        alertDialog.setCanceledOnTouchOutside(false)
         alertDialog.show()
-    }*/
+    }
+
+    private fun addPelliculeToFirestore(name: String, iso: String): Boolean {
+        if (name.length !in 5..40) {
+            Toast.makeText(
+                this,
+                "Le nom de la pellicule doit être compris entre 5 et 40 charactères",
+                Toast.LENGTH_LONG
+            ).show()
+            return false
+        }
+
+        val pellicule = HashMap<String, String>()
+        pellicule["name"] = name
+        pellicule["iso"] = iso
+        pellicule["userId"] = userId.toString()
+
+
+        db.collection(PELLICULE_VALUE)
+            .add(pellicule)
+            .addOnSuccessListener {
+                Toast.makeText(this, "Ajouté avec succès", Toast.LENGTH_LONG).show()
+                val pelliculeAdded = Pellicule(it.id, userId.toString(), name, iso)
+                if (noElement) {
+                    pelliculeList.add(pelliculeAdded)
+                    adapter.updatePelliculeList(pelliculeList)
+                    pellicule_recycler_view.adapter = adapter
+                    pellicule_recycler_view.addOnScrollListener(CustomScrollListener(this))
+                    pellicule_recycler_view.layoutManager = LinearLayoutManager(this)
+                    empty_pellicule_layout.hide()
+                } else {
+                    pelliculeList.add(pelliculeAdded)
+                    adapter.updatePelliculeList(pelliculeList)
+                    adapter.notifyDataSetChanged()
+                }
+            }
+            .addOnFailureListener {
+                Toast.makeText(this, "Le document n'a pas pu être enregistré", Toast.LENGTH_LONG)
+                    .show()
+            }
+        return true
+    }
 
 }
